e7c6bb03dacae0ec95bb9ac5d445aab279e1c4aa
Vinícius Fernandes <viniciusfernandesdev@gmail.com>
2023-07-10 22:33:24 -0300

    feat(#27): challenge #27
    404 page creation

--- src/app/challenges/27/layout.tsx
+++ src/app/challenges/27/layout.tsx
@@ -1,26 +1,34 @@
+'use client'
+
 import { Outfit } from "next/font/google"
 import num from './assets/number.svg'
 import Image from "next/image"
 
 import './style.css'
 
+import { useRouter } from "next/navigation"
+import Link from "next/link"
+
 const outfit = Outfit({ subsets: ['latin'] })
 
 export default function Page() {
+
+    const router = useRouter()
+
     return (
-        <div className={`${outfit.className} p-12 bg-[#000013] w-screen h-screen text-white grid grid-cols-2 items-center justify-center`}>
-            <div className="p-3 w-5/6">
+        <div className={`${outfit.className}  p-4 md:p-12 gap-10 bg-[#000013] w-screen h-screen text-white grid grid-cols-1 md:grid-cols-2 items-center justify-center`}>
+            <div className="md:order-2 justify-center p-6 gap-0 md:gap-2 items-center inline-flex drop-shadow-404">
+                <Image src={num} alt="number" width={80} height={80} className="md:w-full w-2/3"/>
+                <iframe src="https://lottie.host/?file=ab7a5c4d-d0de-4877-be5e-8cffedab2bf1/f7kJBYBWYz.json" width={230} height={230} className="md:w-full w-2/3"></iframe>
+                <Image src={num} alt="number" width={80} height={80} className="md:w-full w-2/3"/>
+            </div>
+            <div className="md:order-1 p-3 w-5/6">
                 <h1 className="text-3xl p-3 font-bold">Ops, esta página não foi encontrada</h1>
                 <h3 className="p-3 text-[#D6D6F2]">Parece que você se perdeu... Tente voltar para a página anterior ou acessar a home.</h3>
                 <div className="flex gap-3 p-3 text-sm">
-                    <button className="bg-[#151557] hover:bg-[#212187] transition-all p-4 rounded-full uppercase">Voltar</button>
-                    <button className="bg-[#151557] hover:bg-[#212187] transition-all p-4 rounded-full uppercase">Ir para a home</button>
+                    <button onClick={() => router.back()} className="bg-[#151557] hover:bg-[#212187] transition-all p-4 rounded-full uppercase">Voltar</button>
+                    <Link href="/" className="bg-[#151557] hover:bg-[#212187] transition-all p-4 rounded-full uppercase">Ir para a home</Link>
                 </div>
-            </div>
-            <div className="justify-center gap-2 items-center inline-flex drop-shadow-404">
-                <Image src={num} alt="number" width={80} height={80} className="bg-transparent"/>
-                <iframe src="https://lottie.host/?file=ab7a5c4d-d0de-4877-be5e-8cffedab2bf1/f7kJBYBWYz.json" width={230} height={230}></iframe>
-                <Image src={num} alt="number" width={80} height={80}/>
             </div>
         </div>
     )
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -7,9 +7,9 @@ export default function Card(
         { title: string, number: number, description: string }
 ) {
     return (
-        <Link href={`/challenges/${number}`} target="_blank" className="drop-shadow-[0_0_35px_rgba(0,0,0,0.25)] w-96 aspect-video">
-            <div className="relative w-full max-w-[350px] border-2 border-white/30 group text-white hover:shadow-lg hover:-translate-y-4 hover:translate-x-4 transition-all">
-            <Image className="w-full aspect-auto grayscale group-hover:grayscale-0 transition-all" src={`/images/${number}.png`} width={250} height={200} alt="fundo" />
+        <Link href={`/challenges/${number}`} className="drop-shadow-[0_0_35px_rgba(0,0,0,0.25)] max-w-96 min-h-44 aspect-video">
+            <div className="relative w-full max-w-[350px] border-2 border-white/30 group text-white hover:shadow-lg hover:-translate-y-3 hover:translate-x-3 transition-all">
+            <Image className="w-full aspect-auto md:grayscale group-hover:grayscale-0 transition-all" src={`/images/${number}.png`} width={250} height={200} alt="fundo" />
                 <div className="absolute bottom-2 overflow-auto m-1">
                     <h1 className="bg-black w-fit p-1 ">
                         <span className="text-2xl">#{number}</span> {title}
